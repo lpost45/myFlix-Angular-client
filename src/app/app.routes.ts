be53d4e2d64a29ea2cd3109701d6611559b2f440
lpost45 <lpost45@live.com>
2025-04-10 18:01:21 -0600

    Exercise 6.4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
+import { ProfileViewComponent } from './profile-view/profile-view.component';
 
 export const routes: Routes = [
     { path: 'welcome', component: WelcomePageComponent },
     { path: 'movies', component: MovieCardComponent },
+    { path: 'profile', component: ProfileViewComponent },
     {path: '', redirectTo: 'welcome', pathMatch: 'full'}
 ];
